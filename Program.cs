51b290a57ea6e35d7f235473c5a7c7600c756dc0
mancymani <72582536+mancymani@users.noreply.github.com>
2021-01-18 20:15:32 -0500

    Add files via upload
    Events

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FinalExamEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            human hr = new human();
+
+        }
+    }
+
+    public class human
+    {
+        public void awake(object sender, ABCEvent ABC)
+        {
+            Console.WriteLine($"Hi The time is {ABC.Anydate}");
+        }
+    }
+
+    public delegate void ABCdelegate(object sender, ABCEvent ABC);
+    public class ABCEvent : EventArgs
+    {
+        public DateTime Anydate { get; set; }
+
+        public ABCEvent(DateTime Anytime)
+        {
+            Anydate = Anytime;
+        }
+    }
+}
